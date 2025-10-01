fc9a91b41f571c75a287449bcb0fd5a0fde91bb5
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-10-01 16:56:09 -0700

    #3.4 News Reader Crew - tools

--- new-reader-agent/main.py
+++ new-reader-agent/main.py
@@ -5,6 +5,7 @@ dotenv.load_dotenv()
 
 from crewai import Crew, Agent, Task
 from crewai.project import CrewBase, crew, agent, task
+from tools import search_tool, scrape_tool
 
 @CrewBase
 class NewsReaderAgent:
@@ -12,13 +13,15 @@ class NewsReaderAgent:
 	@agent
 	def news_hunter_agent(self):
 		return Agent(
-			config=self.agents_config["news_hunter_agent"]
+			config=self.agents_config["news_hunter_agent"],
+			tools=[ search_tool, scrape_tool]
 		)
 
 	@agent
 	def summarizer_agent(self):
 		return Agent(
-			config=self.agents_config["summarizer_agent"]
+			config=self.agents_config["summarizer_agent"],
+			tools=[scrape_tool]
 		)
 
 	@agent
@@ -53,4 +56,13 @@ class NewsReaderAgent:
 			verbose=True
 		)
 
-NewsReaderAgent().crew().kickoff()
+NewsReaderAgent().crew().kickoff(inputs={ "topic": "Stable coin" }) # tasks.yaml 보면 topic 받아야하는 걸로 되어있어
+
+# class 안에 있는 tasks들은 자동으로 1번째 task가 끝나면 그 output이 다음 task의 input으로 가고 가고 가고.. 
+# 마지막 output으로 뭔가를 하고 싶으면 ? 
+
+response = NewsReaderAgent().crew().kickoff(inputs={ "topic": "Stable coin" })
+
+# for task_output in response.tasks_output: # tasks_output 접근 방법, type=list
+# 	# for문으로 개별 결과물 확인 가능
+# 	print(task_output)
--- new-reader-agent/tools.py
+++ new-reader-agent/tools.py
@@ -1,1 +1,67 @@
-from crewai.tools import tool
+import time
+from crewai_tools import tool, SerperDevTool
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup # beautifulsoup: HTML을 다루고 조작하는 거, 태그 찾고, 바꾸고 지우고 등
+
+search_tool = SerperDevTool(
+  n_results=5
+)
+
+search_tool.run(search_query="Cambodia Thai War")
+
+@tool
+def scrape_tool(url:str):
+  """
+    Use this when you need to read the content of a website.
+    Returns the content of a website, in case the website is not available, it returns 'No content'.
+    Input should be a `url` string. for example (https://www.reuters.com/world/asia-pacific/cambodia-thailand-begin-talks-malaysia-amid-fragile-ceasefire-2025-08-04/)
+  """
+
+  print(f"Scrapping URL: {url}")
+
+  with sync_playwright as p:
+    # 브라우저 키고, 페이지 이동, 그 페이지에서 html 추출하고, 정리(header, footer, nav 등 필요 하지 않는 거 정리)
+
+    browser = p.chromium.launch(headless=True) # 브라우저 창 없이 백그라운드에서 실행하기
+
+    page = browser.new_page()
+
+    page.goto(url)
+
+    time.sleep(5) # 모두 로드 될때까지 조금 기다리는거
+
+    html = page.content()
+
+    browser.close()
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    unwated_tags = [
+      "header",
+      "footer",
+      "nav",
+      "aside",
+      "script",
+      "style",
+      "noscript",
+      "iframe",
+      "form",
+      "button",
+      "input",
+      "select",
+      "textarea",
+      "img",
+      "svg",
+      "canvas",
+      "audio",
+      "video",
+      "embed",
+      "object"
+    ]
+
+    for tag in soup.find_all(unwated_tags):
+      tag.decompose() # decompose (통째로 삭제) 페이지 요소와 그 자식들까지 없애줌
+
+    content = soup.get_text(separator=" ")
+
+    return content if content != "" else "No content"
