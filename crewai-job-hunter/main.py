f5704e324649065d3c9faa825344e0768c25e727
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-10-02 09:50:00 -0700

    #4.2 Context and Structured Outputs

--- crewai-job-hunter/main.py
+++ crewai-job-hunter/main.py
@@ -1,6 +1,99 @@
-def main():
-    print("Hello from crewai-job-hunter!")
+import dotenv
 
+dotenv.load_dotenv()
 
-if __name__ == "__main__":
-    main()
+from crewai import Crew, Task, Agent
+from crewai.project import CrewBase, task, agent, crew
+
+@CrewBase
+class JobHunterCrew:
+
+  @agent
+  def job_search_agent(self):
+    return Agent(
+      config=self.agents_config["job_search_agent"]
+    )
+
+  @agent
+  def job_matching_agent(self):
+    return Agent(
+      config=self.agents_config["job_matching_agent"]
+    )
+
+  @agent
+  def resume_optimization_agent(self):
+    return Agent(
+      config=self.agents_config["resume_optimization_agent"]
+    )
+
+  @agent
+  def company_research_agent(self):
+    return Agent(
+      config=self.agents_config["company_research_agent"]
+    )
+
+  @agent
+  def interview_prep_agent(self):
+    return Agent(
+      config=self.agents_config["interview_prep_agent"]
+    )
+
+  @task
+  def job_extraction_task(self):
+    return Task(
+      config=self.tasks_config["job_extraction_task"]
+    )
+
+  @task
+  def job_extraction_task(self):
+    return Task(
+      config=self.tasks_config["job_extraction_task"]
+    )
+
+  @task
+  def job_matching_task(self):
+    return Task(
+      config=self.tasks_config["job_matching_task"]
+    )
+
+  @task
+  def job_selection_task(self):
+    return Task(
+      config=self.tasks_config["job_selection_task"]
+    )
+
+  @task
+  def resume_rewriting_task(self):
+    return Task(
+      config=self.tasks_config["resume_rewriting_task"]
+    )
+
+  @task
+  def company_research_task(self):
+    return Task(
+      config=self.tasks_config["company_research_task"],
+      context=[
+        self.job_selection_task(),
+      ]
+    )
+
+  @task
+  def interview_prep_task(self):
+    return Task(
+      config=self.tasks_config["interview_prep_task"],
+      context=[
+        self.job_selection_task(),
+        self.resume_rewriting_task(),
+        self.company_research_task()
+      ]
+    )
+
+  @crew
+  def crew(self):
+    return Crew(
+      agents=self.agents,
+      tasks=self.tasks,
+      verbose=True
+    )
+
+JobHunterCrew().crew().kickoff()
